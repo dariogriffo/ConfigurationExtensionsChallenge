e2074839ddc74be28dfd50e1c0509abff66a49c1
Renan Martins <renanmartins1999@hotmail.com>
2023-07-21 22:31:59 -0300

    [REFACTOR] Using expression bodied method

--- src/YamlFile/YamlFileConfigurationSource.cs
+++ src/YamlFile/YamlFileConfigurationSource.cs
@@ -5,15 +5,11 @@ namespace YamlFile
     public class YamlFileConfigurationSource : IConfigurationSource
     {
         private readonly YamlFileConfigurationProvider _provider;
-
         public YamlFileConfigurationSource(YamlFileConfigurationProvider provider)
-        {
+        { 
             _provider = provider;
         }
 
-        public IConfigurationProvider Build(IConfigurationBuilder builder)
-        {
-            return _provider;
-        }
+        public IConfigurationProvider Build(IConfigurationBuilder builder) => _provider;
     }
 }
