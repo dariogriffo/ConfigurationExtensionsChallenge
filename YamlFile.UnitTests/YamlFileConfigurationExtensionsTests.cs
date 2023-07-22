a7182d9757e31da750247b344940f0c843cd3c2c
Renan Martins <renanmartins1999@hotmail.com>
2023-07-21 22:21:37 -0300

    [TEST] Retrieving second property value in Yaml File

--- YamlFile.UnitTests/YamlFileConfigurationExtensionsTests.cs
+++ YamlFile.UnitTests/YamlFileConfigurationExtensionsTests.cs
@@ -6,10 +6,17 @@ namespace YamlFile.UnitTests
     public class YamlFileConfigurationExtensionsTests
     {
         [Fact]
-        public void AddYamlFile_When_Has_Basic_Properties_Values()
+        public void AddYamlFile_When_Has_One_Property_Value()
         {
             var configuration = new ConfigurationBuilder().AddYamlFile().Build();
             configuration["first_team"].Should().Be("brazil");
+        }
+
+        [Fact]
+        public void AddYamlFile_When_Has_MoreThanOne_Properties_Values()
+        {
+            var configuration = new ConfigurationBuilder().AddYamlFile().Build();
+            configuration["second_team"].Should().Be("germany");
         }
     }
 }
