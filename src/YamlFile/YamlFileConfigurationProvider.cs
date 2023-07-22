9b7bd395c64016d4ece908b8bad89754202792f4
Renan Martins <renanmartins1999@hotmail.com>
2023-07-21 22:22:50 -0300

    [FEAT] Implementing provider for yaml files

--- src/YamlFile/YamlFileConfigurationProvider.cs
+++ src/YamlFile/YamlFileConfigurationProvider.cs
@@ -1,7 +1,61 @@
-﻿namespace YamlFile;
-public class YamlFileConfigurationProvider
+﻿using Microsoft.Extensions.Configuration;
+
+namespace YamlFile;
+public class YamlFileConfigurationProvider : ConfigurationProvider, IDisposable
 {
-    public YamlFileConfigurationProvider() {
+    private readonly string _fileName;
+    private readonly bool _trim;
+    private readonly bool _removeWrappingQuotes;
+    private readonly string _prefix;
+    private readonly FileSystemWatcher _watcher;
+
+    public YamlFileConfigurationProvider(string fileName, bool trim, bool removeWrappingQuotes, string prefix, bool reloadOnChange)
+    {
+        _fileName = fileName;
+        _trim = trim;
+        _removeWrappingQuotes = removeWrappingQuotes;
+        _prefix = prefix;
+
+        _watcher = new FileSystemWatcher(Path.GetDirectoryName(_fileName) ?? ".");
+        _watcher.Changed += new FileSystemEventHandler(OnChanged);
+        _watcher.EnableRaisingEvents = true;
+    }
+
+    private void OnChanged(object sender, FileSystemEventArgs e) {
+        Load();
+        OnReload();
+    }
+
+    public override void Load() {
+        try
+        {
+            LoadData();
+        } 
+        catch(FormatException ex)
+        {
+            throw;
+        }
+    }
+
+    private void LoadData()
+    {
+        var linesWithPropertyValues = 
+            File
+                .ReadAllLines(_fileName)
+                .Where(l => !l.StartsWith("#"));
         
+        string TrimSpaceInTheStart(string line) => line.Split(":").Last().TrimStart();
+        
+        var configuration = 
+            linesWithPropertyValues
+                .Select(l => l.Split(":"))
+                .Select(l => new KeyValuePair<string, string>(l[0], l[1].TrimStart()));
+                
+        Data = configuration.ToDictionary(l => l.Key, l => l.Value);
+    }
+
+    public void Dispose()
+    {
+        _watcher?.Dispose();
     }
 }
