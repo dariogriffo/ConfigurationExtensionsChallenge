4c735ff78608ca5ec09fff71e6f6b1203542eaa2
Renan Martins <renanmartins1999@hotmail.com>
2023-07-25 20:27:48 -0300

    [FEAT] Removing double quotes in values

--- src/YamlFile/YamlFileConfigurationProvider.cs
+++ src/YamlFile/YamlFileConfigurationProvider.cs
@@ -47,7 +47,7 @@ public class YamlFileConfigurationProvider : ConfigurationProvider, IDisposable
         var configuration = 
             linesWithPropertyValues
                 .Select(l => l.Split(":"))
-                .Select(l => new KeyValuePair<string, string>(l[0], l[1].TrimStart()));
+                .Select(l => new KeyValuePair<string, string>(l[0], l[1].TrimStart().Trim('\"')));
                 
         Data = configuration.ToDictionary(l => l.Key, l => l.Value);
     }
